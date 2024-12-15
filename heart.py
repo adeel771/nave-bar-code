8da76aa7d0e8bcbeba3f03a7b120365b057b9c86
adeel771 <muhammad.adeel.rajpoot771@gmail.com>
2024-12-15 21:56:16 +0500

    Add files via upload

---
+++ heart.py
@@ -0,0 +1,23 @@
+# import math
+# from turtle import *
+
+# # Define the x-coordinate function
+# def hearta(k):
+#     return 15*math.sin(k)**3
+
+# # Define the y-coordinate function
+# def heartb(k):
+#     return 12 * math.cos(k) - 5 * \
+#            math.cos(2 * k) - 2 * \
+#            math.cos(3 * k) - \
+#            math.cos(4 * k)
+
+# # Turtle settings
+# speed(0)  #
+name="muhammad adeel"
+age=20
+gander="male"
+print(type(name))
+print("my age is:",age)
+print(gander)
+
